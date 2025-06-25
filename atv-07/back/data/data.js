33fa7ab0ce6d974e46cba04d8d1212414642e204
DevKaioD <kaiodavidson@gmail.com>
2025-06-25 08:33:07 -0300

    Back att: data att

--- atv-07/back/data/data.js
+++ atv-07/back/data/data.js
@@ -3,31 +3,31 @@ const data = [
         id: 1,
         nome: 'João Silva',
         curso: 'Engenharia de Software',
-        ira: 8.5
+        ira: 8.55
     },
     {
         id: 2,
         nome: 'Maria Oliveira',
         curso: 'Ciência da Computação',
-        ira: 9.0
+        ira: 8.77
     },
     {
         id: 3,
         nome: 'Pedro Santos',
         curso: 'Sistemas de Informação',
-        ira: 7.8
+        ira: 7.89
     },
     {
         id: 4,
         nome: 'Ana Costa',
         curso: 'Engenharia de Produção',
-        ira: 8.2
+        ira: 8.20
     },
     {
         id: 5,
         nome: 'Lucas Pereira',
         curso: 'Engenharia Elétrica',
-        ira: 9.5
+        ira: 9.50
     }
 ];
 
