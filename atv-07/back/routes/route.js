111ff880e2c52c5c64758b0a6ad2476a0eccc150
DevKaioD <kaiodavidson@gmail.com>
2025-06-24 23:18:49 -0300

    Back att: routes.js att and correction

--- atv-07/back/routes/route.js
+++ atv-07/back/routes/route.js
@@ -1,57 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const alunos = require('../data/data');
-const Aluno = require('../models/aluno');
+const AlunoService = require('../services/alunoService.js');
 
 router.get('/listar', (req, res) => {
-    res.json(alunos);
-});
-
-router.get('/media', (req, res) => {
-    const totalIra = alunos.reduce((acc, aluno) => acc + aluno.ira, 0);
-    const mediaIra = totalIra / alunos.length;
-
-    res.json({ mediaIra });
+    const lista = AlunoService.listarAlunos();
+    res.json(lista);
 });
 
 router.post('/criar', (req, res) => {
-    const json = req.body;
-    let id = (alunos[alunos.length - 1].id) + 1;
-    
-    const novoAluno = new Aluno(id++, json.nome, json.curso, json.ira);
+    const { nome, curso, ira } = req.body;
 
-    alunos.push(novoAluno);
-
-    res.status(201).json({mensagem: "Aluno cadastrado com sucesso", aluno: novoAluno});
+    const aluno = AlunoService.cadastrar({nome, curso, ira});
+    res.status(201).json({ mensagem: "Aluno cadastrado com sucesso", aluno });
 });
 
 router.put('/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const { nome, curso, ira } = req.body;
 
-    if (!nome || !curso || !ira) {
-        return res.status(400).json({ mensagem: "Todos os campos são obrigatórios" });
-    }
+    const alunoAtualizado = AlunoService.atualizar(id, { nome, curso, ira });
 
-    const alunoIndex = alunos.findIndex(aluno => aluno.id === id);
-
-    if (alunoIndex === -1) {
-        return res.status(404).json({ mensagem: "Aluno não encontrado" });
-    }
-
-    alunos[alunoIndex] = new Aluno(id, nome, curso, ira);
-    res.json({ mensagem: "Aluno atualizado com sucesso", aluno: alunos[alunoIndex] });
+    res.json({ mensagem: "Aluno atualizado com sucesso", aluno: alunoAtualizado });
 });
 
 router.delete('/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    const alunoIndex = alunos.findIndex(aluno => aluno.id === id);
+    const alunoRemovido = AlunoService.remover(id);
 
-    if (alunoIndex === -1) {
-        return res.status(404).json({ mensagem: "Aluno não encontrado" });
-    }
-
-    alunos.splice(alunoIndex, 1);
     res.json({ mensagem: "Aluno removido com sucesso" });
 });
 
